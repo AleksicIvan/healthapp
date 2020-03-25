0704390f0e6ab85d798aa60f240d964585645886
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2020-03-25 18:35:38 +0100

    Add JWT constants class

---
+++ src/main/java/com/aleksic/medapp/JwtProperties.java
@@ -0,0 +1,8 @@
+package com.aleksic.medapp;
+
+public class JwtProperties {
+    public static final String SECRET = "redroom666";
+    public static final int EXPIRATION_TIME = 864000000; // 10 days
+    public static final String TOKEN_PREFIX = "Bearer";
+    public static final String HEADER_STRING = "Authorization";
+}
