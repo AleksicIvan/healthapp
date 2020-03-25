c78992f58b34fbe4111a84bdd847ec7fccdfe85b
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2020-03-25 18:16:17 +0100

    Refactor WebSecurityConfig

---
+++ src/main/java/com/aleksic/medapp/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package com.aleksic.medapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Qualifier("customerUserDetailsService")
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http .authorizeRequests()
+                .antMatchers("/api/lekari") .hasRole("USER")
+                .antMatchers("/api/specijalizacije") .hasRole("USER")
+                .antMatchers("/api/users") .hasRole("ADMIN")
+                .antMatchers("/").permitAll()
+                .and()
+                .formLogin()
+                .and()
+                .rememberMe().tokenValiditySeconds(2592000);
+    }
+
+}
