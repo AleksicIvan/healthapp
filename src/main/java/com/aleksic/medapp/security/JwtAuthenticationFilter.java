d4fd62fa1d6e4143e44d0c4a69ca32a837e9fd79
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2020-03-26 08:12:00 +0100

    Complete JWT cofniguration

---
+++ src/main/java/com/aleksic/medapp/security/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.aleksic.medapp.security;
+
+import com.aleksic.medapp.models.LoginViewModel;
+import com.aleksic.medapp.models.User;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private AuthenticationManager authenticationManager;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        // Grab credentials and map them to login viewmodel
+        LoginViewModel credentials = null;
+        try {
+            credentials = new ObjectMapper().readValue(request.getInputStream(), LoginViewModel.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Create login token
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                credentials.getUsername(),
+                credentials.getPassword(),
+                new ArrayList<>());
+
+        // Authenticate user
+        Authentication auth = authenticationManager.authenticate(authenticationToken);
+
+        return auth;
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        // Grab principal
+        User principal = (User) authResult.getPrincipal();
+
+        // Create JWT Token
+        String token = JWT.create()
+                .withSubject(principal.getUsername())
+                .withExpiresAt(new Date(System.currentTimeMillis() + JwtProperties.EXPIRATION_TIME))
+                .sign(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()));
+
+        // Add token in response
+        response.addHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX +  token);
+    }
+}
--- src/main/java/com/aleksic/medapp/security/WebSecurityConfig.java
+++ src/main/java/com/aleksic/medapp/security/WebSecurityConfig.java
@@ -1,13 +1,16 @@
-package com.aleksic.medapp;
+package com.aleksic.medapp.security;
 
+import com.aleksic.medapp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -19,6 +22,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Qualifier("customerUserDetailsService")
     @Autowired
     private UserDetailsService userDetailsService;
+    @Autowired
+    private UserRepository userRepository;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -34,15 +39,25 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http .authorizeRequests()
+        http
+                // remove csrf and state in session because in jwt we do not need them
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                // add jwt filters (1. authentication, 2. authorization)
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
+                .addFilter(new JwtAuthorizationFilter(authenticationManager(),  userRepository))
+                .authorizeRequests()
+                // configure access rules
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .antMatchers("/api/lekari") .hasRole("USER")
                 .antMatchers("/api/specijalizacije") .hasRole("USER")
                 .antMatchers("/api/users") .hasRole("ADMIN")
                 .antMatchers("/").permitAll()
-                .and()
-                .formLogin()
-                .and()
-                .rememberMe().tokenValiditySeconds(2592000);
+                .anyRequest().authenticated();
+        //                .formLogin()
+//                .and()
+//                .rememberMe().tokenValiditySeconds(2592000);
     }
 
 }
