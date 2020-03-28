6cf8882b84ebc6e9727aaca820c38239219b18eb
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2020-03-28 08:28:12 +0100

    Add unit test for UnserController class

--- src/test/java/com/aleksic/medapp/controllers/UserControllerTest.java
+++ src/test/java/com/aleksic/medapp/controllers/UserControllerTest.java
@@ -4,23 +4,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.hamcrest.Matchers.containsString;
+import static org.assertj.core.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-//@AutoConfigureMockMvc
-//@WebMvcTest
-public class HomeControllerTest {
+class UserControllerTest {
     @Autowired
-    private HomeController controller;
+    private UserController controller;
 
     @Autowired
     private WebApplicationContext context;
@@ -37,12 +36,15 @@ public class HomeControllerTest {
         assertThat(controller).isNotNull();
     }
 
+
     @Test
-    public void shouldReturnDefaultFromHome() throws Exception {
-        mockMvc.perform(get("/"))
+    @WithMockUser(username="admin",roles={"ADMIN"})
+    void getAllUsers() throws Exception {
+        MvcResult result = mockMvc
+                .perform(get("/api/users").contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(content().string(containsString("Hello from HomeController")));
+                .andReturn();
+        assertThat(result.getResponse().getStatus()).isEqualTo(200);
     }
-
 }
