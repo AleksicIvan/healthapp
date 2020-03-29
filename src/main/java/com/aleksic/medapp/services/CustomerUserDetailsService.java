12fc548468f75eec3d03eac43c82609470bfb2ce
Ivan Aleksic <ivan.aleksic@guidefitter.com>
2020-03-29 19:44:42 +0200

    Refactor CustomerUserDetailsService to use password encoder; provide getAllUsers and saveUser methods

--- src/main/java/com/aleksic/medapp/services/CustomerUserDetailsService.java
+++ src/main/java/com/aleksic/medapp/services/CustomerUserDetailsService.java
@@ -6,6 +6,7 @@ import com.aleksic.medapp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,11 +14,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import java.util.Collection;
+import java.util.List;
 
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public List<User> getAllUsers () {
+        return userRepository.findAll();
+    }
+
+    public User saveUser (User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
